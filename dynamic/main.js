057eed832224d3fa85e6fd514f88d07cf6c826ae
onokatio <onokatio@gmail.com>
2019-10-16 16:35:05 +0900

    create list json

--- dynamic/main.js
+++ dynamic/main.js
@@ -5,16 +5,23 @@ fs.readdir('../markdown')
 	.then( (files) => {
 		files = files.filter( (filename) => filename.endsWith('.md') )
 		files = files.filter( (filename) => filename !== ('index.md') )
-		console.log(files)
-		contents = files.map( (filename) => {
-			fs.readFile('../markdown/' + filename, "utf-8").then( (content) => {
+		readPromises = files.map( (filename) => {
+			return fs.readFile('../markdown/' + filename, "utf-8")
+				.then( (content) => Promise.resolve([content, filename]) )
+		})
+		Promise.all(readPromises).then( (contents) => {
+			const summaries = contents.map( (contentAndFilename) => {
+				content = contentAndFilename[0]
+				filename = contentAndFilename[1]
 				content = content.replace(/^.+\n=+/,'') // remove (title \n ===)
 					.replace(/^# .+\n/,'')          // remove (# title)
 					.replace(/\n#+ /g,'\n')         // remove markdown sharp
 					.replace(/`/g,'')               // remove markdown back quote
 					.replace(/^ +- /g,'')           // remove markdown hyphen
 					.replace(/\n/g,'')              // remove newline
-				console.log('summary = ' + content.slice(0,50))
+				return {  filename: filename, summary: content.slice(0,50)}
 			})
+			json = JSON.stringify(summaries)
+			fs.writeFile('list', json)
 		})
 	} )
