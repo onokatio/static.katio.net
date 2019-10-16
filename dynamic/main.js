f5bcd8b6b5653e9f3d662375053508b836f02055
onokatio <onokatio@gmail.com>
2019-10-16 12:02:44 +0900

    extract summary

--- dynamic/main.js
+++ dynamic/main.js
@@ -1,8 +1,20 @@
 const fetch = require("node-fetch")
+const fs = require('fs').promises
 
-fetch('https://api.github.com/repos/onokatio-blog/blog/contents/markdown')
-	.then( (response) => response.json() )
-	.then( (json) => {
-		urlList = json.map( (file) => { return { path: file.path, download_url: file.download_url } } )
-		console.log(urlList)
-	})
+fs.readdir('../markdown')
+	.then( (files) => {
+		files = files.filter( (filename) => filename.endsWith('.md') )
+		files = files.filter( (filename) => filename !== ('index.md') )
+		console.log(files)
+		contents = files.map( (filename) => {
+			fs.readFile('../markdown/' + filename, "utf-8").then( (content) => {
+				content = content.replace(/^.+\n=+/,'') // remove (title \n ===)
+					.replace(/^# .+\n/,'')          // remove (# title)
+					.replace(/\n#+ /g,'\n')         // remove markdown sharp
+					.replace(/`/g,'')               // remove markdown back quote
+					.replace(/^ +- /g,'')           // remove markdown hyphen
+					.replace(/\n/g,'')              // remove newline
+				console.log('summary = ' + content.slice(0,50))
+			})
+		})
+	} )
