a9a5a53f3710adcfe6c84b6d4b6a0df0ec73c2a2
onokatio <onokatio@gmail.com>
2019-10-16 10:35:20 +0900

    map list

--- dynamic/main.js
+++ dynamic/main.js
@@ -3,5 +3,6 @@ const fetch = require("node-fetch")
 fetch('https://api.github.com/repos/onokatio-blog/blog/contents/markdown')
 	.then( (response) => response.json() )
 	.then( (json) => {
-		console.log(json)
+		urlList = json.map( (file) => { return { path: file.path, download_url: file.download_url } } )
+		console.log(urlList)
 	})
